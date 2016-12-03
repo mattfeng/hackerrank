8c9c32e2e4dc73696edffe3c628f360990454253
Matthew Feng <matthew.f.phs@gmail.com>
2016-12-02 18:42:27 -0800

    solved conditionals

---
+++ cpp/intro/conditionals.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int val;
+
+	cin >> val;
+
+	if (val == 1) {
+		printf("one\n");
+	}
+	else if (val == 2) {
+		printf("two\n");
+	}
+	else if (val == 3) {
+		printf("three\n");
+	}
+	else if (val == 4) {
+		printf("four\n");
+	}
+	else if (val == 5) {
+		printf("five\n");
+	}
+	else if (val == 6) {
+		printf("six\n");
+	}
+	else if (val == 7) {
+		printf("seven\n");
+	}
+	else if (val == 8) {
+		printf("eight\n");
+	}
+	else if (val == 9) {
+		printf("nine\n");
+	}
+	else {
+		printf("Greater than 9\n");
+	}
+ 
+   	return 0;
+}
